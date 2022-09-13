ad779d9d0a1ed21ba76d5e13bf0f67b8a1c4a5ed
mbakin <mehmetberkakinn@outlook.com>
2022-09-13 12:02:10 +0300

    [#12]feat: binding json to struct validation

--- account/handler/signup.go
+++ account/handler/signup.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type signupReq struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,gte=6,lte=30"`
+}
+
+// Signup handler
+func (h *Handler) Signup(c *gin.Context) {
+	// define a variable to which we'll bind incoming
+	// json body, {email, password}
+	var req signupReq
+
+	// Bind incoming json to struct and check for validation errors
+	if ok := bindData(c, &req); !ok {
+		return
+	}
+}
