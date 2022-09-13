e8fc174dd3c65730307a630804a5ea8fd9b8c3a4
mbakin <mehmetberkakinn@outlook.com>
2022-09-13 12:08:33 +0300

    [#12]feat: create signup handler

--- account/handler/signup.go
+++ account/handler/signup.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mbakin/custom-dictionary/account/model"
+	"github.com/mbakin/custom-dictionary/account/model/apperrors"
+	"log"
 )
 
 type signupReq struct {
@@ -17,6 +20,21 @@ func (h *Handler) Signup(c *gin.Context) {
 
 	// Bind incoming json to struct and check for validation errors
 	if ok := bindData(c, &req); !ok {
+		return
+	}
+
+	u := &model.User{
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	err := h.UserService.Signup(c, u)
+
+	if err != nil {
+		log.Printf("Failed to sign up user: %v\n", err.Error())
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err,
+		})
 		return
 	}
 }
