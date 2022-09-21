ba95e7bc7e1a9ffe4175f983a3a89ac7ae9e8e00
mbakin <mehmetberkakinn@outlook.com>
2022-09-21 19:56:58 +0300

    [#15]: implement tokens method

--- account/service/token_service.go
+++ account/service/token_service.go
@@ -3,6 +3,10 @@ package service
 import (
 	"crypto/rsa"
 	"github.com/mbakin/custom-dictionary/account/model"
+	"github.com/mbakin/custom-dictionary/account/model/apperrors"
+	"log"
+
+	"context"
 )
 
 // TokenService used for injecting an implementation of TokenRepository
@@ -32,4 +36,31 @@ func NewTokenService(c *TSConfig) model.TokenService {
 		PubKey:        c.PubKey,
 		RefreshSecret: c.RefreshSecret,
 	}
+}
+
+// NewPairFromUser creates fresh id and refresh tokens for the current user
+// If a previous token is included, the previous token is removed from
+// the tokens repository
+func (s *TokenService) NewPairFromUser(ctx context.Context, u *model.User, prevTokenID string) (*model.TokenPair, error) {
+	// No need to use a repository for idToken as it is unrelated to any data source
+	idToken, err := generateIDToken(u, s.PrivKey)
+
+	if err != nil {
+		log.Printf("Error generating idToken for uid: %v. Error: %v\n", u.UID, err.Error())
+		return nil, apperrors.NewInternal()
+	}
+
+	refreshToken, err := generateRefreshToken(u.UID, s.RefreshSecret)
+
+	if err != nil {
+		log.Printf("Error generating refreshToken for uid: %v. Error: %v\n", u.UID, err.Error())
+		return nil, apperrors.NewInternal()
+	}
+
+	// TODO: store refresh tokens by calling TokenRepository methods
+
+	return &model.TokenPair{
+		IDToken:      idToken,
+		RefreshToken: refreshToken.SS,
+	}, nil
 }
